117cd728536704bc5106148f20db7015ee3d0b24
NekaSidorov <111890852+NekaSidorov@users.noreply.github.com>
2024-07-31 14:02:03 +0300

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { SignUp } from "./components/SignUp/SignUp";
-import { FormUp } from "./components/SignUp/FormUp";
-export default function Home() {}
--- src/app/types/types.ts
+++
@@ -1,8 +0,0 @@
-
-export type props = {
-    name?: string,
-    id?: number,
-    isOpened?: boolean,
-    children?: React.ReactNode,
-    close?: VoidFunction
-}
